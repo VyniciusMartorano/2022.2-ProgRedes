37363d715c021621a208c4c02d9dd215f595405e
charles-freitas <ccmfreitas@gmail.com>
2022-10-13 15:16:56 -0300

    Atualização do Repositório

--- 2022_10_13/questao_anp.py
+++ 2022_10_13/questao_anp.py
@@ -1,49 +1,41 @@
 import os, sys 
-import questao_anp_lib as anp_lib
-
-# Obtendo o diretório onde o programa .PY está salvo
-diretorio_app   = os.path.dirname(os.path.abspath(__file__)) 
-
-# Montando o diretório onde os dados fonte estão guardados
-diretorio_dados = diretorio_app + '\\serie_historica_anp'
-
-# Montando o diretório onde os dados estatisticos serão salvos
-diretorio_estat = diretorio_app + '\\dados_estatisticos'
+from questao_anp_lib import *
+from questao_anp_constantes import *
 
 # Montando uma lista com os arquivos do diretório de dados
 try:
-    lista_arquivos = os.listdir(diretorio_dados)
+    lista_arquivos = os.listdir(DIR_DADOS)
 except:
-    print(f'\nErro na Leitura do Diretório...{sys.exc_info()[0]}\n')
+    print(f'\n{COR_ERRO}ERRO: Leitura do Diretório...{sys.exc_info()[0]}{COR_PADRAO}\n')
     sys.exit()
 else:
     if len(lista_arquivos) == 0:
-        print('\nO Diretório está vazio...\n')
+        print(f'\n{COR_ERRO}ERRO: O Diretório está vazio...{COR_PADRAO}\n')
         sys.exit()
 
 # Lendo os arquivos
 lista_dados = list()
 for arquivo in lista_arquivos:
-    print('\nLendo Arquivo: {0}'.format(arquivo))
-    nome_arquivo = diretorio_dados + '\\' + arquivo
-    lido, lista_auxiliar, erro = anp_lib.ler_arquivo(nome_arquivo,';',True)
+    print(f'\nLendo Arquivo: {COR_MENSAGEM}{arquivo}{COR_PADRAO}')
+    nome_arquivo = DIR_DADOS + '\\' + arquivo
+    lido, lista_auxiliar, erro = ler_arquivo(nome_arquivo,SEPARADOR,True)
     if lido:
-        print('Lido com Sucesso...')
+        print(f'{COR_ACERTO}Lido com Sucesso...{COR_PADRAO}')
         lista_dados.extend(lista_auxiliar) 
     else:
-        print(f'Erro na Leitura...{erro}')
+        print(f'{COR_ERRO}ERRO: Erro na Leitura do Arquivo...{erro}{COR_PADRAO}')
 
 # Criando o diretório onde os dados serão salvos
 try:
-    os.mkdir(diretorio_estat)
+    os.mkdir(DIR_ESTAT)
 except:
-    print('\nDiretório já existe...\n')
+    print(f'\n{COR_ERRO}ERRO: Diretório já existe...{COR_PADRAO}')
 
 # Salvando os dados lidos
-print('\nSalvando Arquivo:')
-nome_arquivo = diretorio_estat + '\\dados_consolidados_anp.txt'
-salvo, erro  = anp_lib.salvar_arquivo(lista_dados, nome_arquivo, ';') 
+print(f'\nSalvando Arquivo:')
+nome_arquivo = DIR_ESTAT + '\\dados_consolidados_anp.txt'
+salvo, erro  = salvar_arquivo(lista_dados, nome_arquivo, SEPARADOR) 
 if salvo:
-    print('Arquivo salvo com Sucesso...\n')
+    print(f'{COR_ACERTO}Arquivo salvo com Sucesso...{COR_PADRAO}\n')
 else:
-    print(f'Erro na Leitura...{erro}')
+    print(f'{COR_ERRO}ERRO: Erro no salvamento do arquivo...{erro}{COR_PADRAO}')
---
+++ 2022_10_13/questao_anp_constantes.py
@@ -0,0 +1,17 @@
+import os
+
+# Obtendo o diretório onde o programa .PY está salvo
+diretorio_app = os.path.dirname(os.path.abspath(__file__)) 
+
+# Definindo as constantes de diretórios
+DIR_DADOS = diretorio_app + '\\serie_historica_anp'
+DIR_ESTAT = diretorio_app + '\\dados_estatisticos'
+
+# Definindo a constante do separador de dados
+SEPARADOR = ';'
+
+# Definindo as constantes das cores de texto
+COR_ERRO     = '\033[40;31;1m'
+COR_ACERTO   = '\033[40;34;1m'
+COR_PADRAO   = '\033[40;37m'
+COR_MENSAGEM = '\033[40;32m'
