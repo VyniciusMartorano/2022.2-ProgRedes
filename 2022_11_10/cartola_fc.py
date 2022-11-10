c813cc7986bce57fcd5a7de8ddad8c09ca1c72ec
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2022-11-10 15:07:45 -0300

    Atualização de Repositório

---
+++ 2022_11_10/cartola_fc.py
@@ -0,0 +1,63 @@
+﻿# -------------------------------------------------------------------------
+# Exemplos de usos 'avançados' de funções para tratamento de dados
+# Por Galileu Batista - IFRN 2019
+# Adaptado por Charles Freitas - IFRN 2019
+# -------------------------------------------------------------------------
+
+import sys
+import os
+import cartola_lib
+
+lstFormacoes = [
+                  ['3-4-3', [3,0,4,3]], ['3-5-2', [3,0,5,2]], 
+                  ['4-3-3', [2,2,3,3]], ['4-4-2', [2,2,4,2]], 
+                  ['4-5-1', [2,2,5,1]], ['5-3-2', [2,3,3,2]], 
+                  ['5-4-1', [3,2,4,1]]
+               ]
+
+# -------------------------------------------------------------------------
+
+# Cria um dicionário com os dados do mercado e extrai os quatro valores
+dicCartolaFC = cartola_lib.jsonCartola()
+
+# Limpando a tela
+os.system('cls')
+
+while True:
+   print('')
+   print('='*100)
+   print('SELECIONE O ESQUEMA TÁTICO')
+   print('-'*100)
+   for i in range(0, len(lstFormacoes)):
+      print(f'[ {i+1} ] - Esquema Tático {lstFormacoes[i][0]}')
+   print('[ 0 ] - SAIR\n')
+
+   try:
+      while True:
+         intEsquema = int(input('Informe o Esquema Tático: '))
+         if (intEsquema >= 1) and (intEsquema <= len(lstFormacoes)): break
+         if (intEsquema == 0): sys.exit()
+   except SystemExit:
+      print('Programa Encerrado...')
+      break
+   except ValueError:
+      print('Valor Informado Inválido...')
+      continue
+   except:
+      print(f'Houve um Erro...{sys.exc_info()[0]}')
+      continue
+
+   # Montando o time
+   print('')
+   print('-'*100)
+   print(f'SELEÇÃO DO CartolaFC - Esquema Tático {lstFormacoes[intEsquema-1][0]}')
+   print('-'*100)
+   time = cartola_lib.escalacao(dicCartolaFC, lstFormacoes[intEsquema-1][1])
+   for i in range(0, len(time)):
+      for j in range(0, len(time[i])):
+         posicao = time[i][j]['posicao_nome'].upper() + ('.'*(10-len(time[i][j]['posicao_nome']))) + ':'
+         atleta  = time[i][j]['apelido'].upper() + ' (' + time[i][j]['nome'] + ')'
+         atleta  = atleta + ('.'*(60-len(atleta))) + ':'
+         clube   = time[i][j]['clube_nome']
+         print(f'{posicao} {atleta} {clube}')
+   print('-'*100)
---
+++ 2022_11_10/cartola_lib.py
@@ -0,0 +1,70 @@
+﻿import requests
+import json
+
+strURL = 'https://api.cartolafc.globo.com/atletas/mercado'
+
+# ----------------------------------------------------------------------------------------
+# Retorna um dicionário com os dados do mercado do Cartola (2019.1)
+# O dicionário está ordenado pela pontuação do atleta deforma decrescente
+def jsonCartola():
+   dic_cartola = requests.get(strURL, verify=True).json()
+   dic_atletas = dic_cartola['atletas']
+   dic_atletas = sorted(dic_atletas, key=lambda a: a['jogos_num'] * a['media_num'], reverse=True)
+
+   global dic_clubes   
+   global dic_posicoes 
+   dic_clubes   = dic_cartola['clubes']
+   dic_posicoes = dic_cartola['posicoes']
+   #dic_status  = dic_cartola['status']
+   return dic_atletas
+
+
+# ----------------------------------------------------------------------------------------
+# Retorna uma lista com os dados do atleta
+def atleta(dicCartola, intPosicao, intQtDados=1):
+   tupla_filtro = list(filter(lambda a: a['posicao_id'] == intPosicao, dicCartola))[0:intQtDados]
+
+   # Transformando a tupla em lista. cada posição da lista é um dicionário
+   lst_auxiliar = []
+   for i in range(0, intQtDados): lst_auxiliar.append(tupla_filtro[i])
+   
+   # Definindo os dados que serão retornados
+   lst_dados_retorno = ['nome', 'apelido', 'clube_id', 'posicao_id', 'media_num', 'jogos_num']
+   lst_retorno = []
+   for i in range(0,len(lst_auxiliar)):
+      dic_auxiliar = {}
+      for key, value in lst_auxiliar[i].items():
+         if (key in lst_dados_retorno): dic_auxiliar[key] = value
+      # Adicionando o nome do clube
+      dic_auxiliar['clube_nome'] = clube(str(dic_auxiliar['clube_id']))
+      # Adicionando a descrição da posição
+      dic_auxiliar['posicao_nome'] = posicao(str(dic_auxiliar['posicao_id']))
+      lst_retorno.append(dic_auxiliar)
+   
+   return lst_retorno
+
+# ----------------------------------------------------------------------------------------
+# Retorna o nome do clube
+def clube(strClubeID):
+   strNomeClube = dic_clubes[strClubeID]['nome']
+   return strNomeClube
+
+# ----------------------------------------------------------------------------------------
+# Retorna a Descrição da Posiçao
+def posicao(strPosicaoID):
+   strNomePosicao = dic_posicoes[strPosicaoID]['nome']
+   return strNomePosicao
+
+# ----------------------------------------------------------------------------------------
+def escalacao(dicCartola, lstFormacao):   
+   # Montando a escalação
+   escalacao = []
+   escalacao.append(atleta(dicCartola, 6)) # Técnico
+   escalacao.append(atleta(dicCartola, 1)) # Goleiro
+   escalacao.append(atleta(dicCartola, 3, lstFormacao[0])) # Zagueiros
+   escalacao.append(atleta(dicCartola, 2, lstFormacao[1])) # Laterais
+   escalacao.append(atleta(dicCartola, 4, lstFormacao[2])) # Meias
+   escalacao.append(atleta(dicCartola, 5, lstFormacao[3])) # Atacantes
+
+   return escalacao
+
